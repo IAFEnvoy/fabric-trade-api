047f735289cf607117680987c0c5293af800e333
IAFEnvoy <tanksheng545@126.com>
2024-07-04 13:34:57 +0800

    0.1.1

--- src/main/java/com/iafenvoy/fabric/trade/villager/offer/TradeOfferHelper.java
+++ src/main/java/com/iafenvoy/fabric/trade/villager/offer/TradeOfferHelper.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.function.Consumer;
 
 // From object builder api v1
+
 /**
  * Utilities to help with registration of trade offers.
  */
@@ -14,19 +15,23 @@ public final class TradeOfferHelper {
     /**
      * Registers trade offer factories for use by villagers.
      *
-     * <p>Below is an example, of registering a trade offer factory to be added a blacksmith with a profession level of 3:
-     * <blockquote><pre>
-     * TradeOfferHelper.registerVillagerOffers(VillagerProfession.BLACKSMITH, 3, factories -> {
-     * 	factories.add(new CustomTradeFactory(...);
-     * });
-     * </pre></blockquote>
+     * @param profession the villager profession to assign the trades to
+     * @param level      the profession level the villager must be to offer the trades
+     * @param factories  a list of trade factory you want to add
+     */
+    public static void registerVillagerOffers(VillagerProfession profession, int level, TradeOffers.Factory... factories) {
+        registerVillagerOffers(profession, level, List.of(factories));
+    }
+
+    /**
+     * Registers trade offer factories for use by villagers.
      *
      * @param profession the villager profession to assign the trades to
      * @param level      the profession level the villager must be to offer the trades
      * @param factories  a list of trade factory you want to add
      */
     public static void registerVillagerOffers(VillagerProfession profession, int level, List<TradeOffers.Factory> factories) {
-        TradeOfferInternals.registerVillagerOffers(profession, level, factory -> factory.addAll(factories));
+        registerVillagerOffers(profession, level, factory -> factory.addAll(factories));
     }
 
     /**
@@ -53,8 +58,18 @@ public final class TradeOfferHelper {
      * @param level     the level the trades
      * @param factories a list of trade factory you want to add
      */
+    public static void registerWanderingTraderOffers(int level, TradeOffers.Factory... factories) {
+        registerWanderingTraderOffers(level, List.of(factories));
+    }
+
+    /**
+     * Registers trade offer factories for use by wandering trades.
+     *
+     * @param level     the level the trades
+     * @param factories a list of trade factory you want to add
+     */
     public static void registerWanderingTraderOffers(int level, List<TradeOffers.Factory> factories) {
-        TradeOfferInternals.registerWanderingTraderOffers(level, factory -> factory.addAll(factories));
+        registerWanderingTraderOffers(level, factory -> factory.addAll(factories));
     }
 
     /**
